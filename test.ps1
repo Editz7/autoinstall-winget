a05fd4ac16f301c9bee0f1480a45c9f6f506aaf9
Editz7 <aeast7@protonmail.com>
2023-04-11 08:39:15 -0500

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,25 +1,27 @@
-# Check if Chocolatey is installed
-if (!(Get-Command choco -ErrorAction SilentlyContinue)) {
-    # Prompt the user to install Chocolatey
-    $installChoco = Read-Host "Chocolatey is not installed. Do you want to install it? (Y/N)"
-    if ($installChoco -eq "Y") {
-        # Download and install Chocolatey
-        Set-ExecutionPolicy Bypass -Scope Process -Force
-        Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-        Write-Host "Chocolatey installed successfully!"
+# Check if Winget is installed
+if (!(Get-Command winget -ErrorAction SilentlyContinue)) {
+    # Prompt the user to install Winget
+    $installWinget = Read-Host "Winget is not installed. Do you want to install it? (Y/N)"
+    if ($installWinget -eq "Y") {
+        # Download and install the Winget MSI from Microsoft's GitHub repository
+        $wingetMsiUrl = "https://github.com/microsoft/winget-cli/releases/latest/download/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
+        $wingetMsiPath = "$env:TEMP\winget.msixbundle"
+        Invoke-WebRequest $wingetMsiUrl -OutFile $wingetMsiPath
+        Add-AppPackage $wingetMsiPath -ErrorAction Stop
+        Write-Host "Winget installed successfully!"
     } else {
-        Write-Host "Chocolatey installation cancelled."
+        Write-Host "Winget installation cancelled."
         exit
     }
 }
 
 # Define a list of applications to install
-$applications = "7zip", "obs-studio", "discord", "chromium", "telegram"
+$applications = "7zip.7zip", "OBSProject.OBSStudio", "Discord.Discord", "Chromium.Chromium", "Telegram.TelegramDesktop"
 
-# Loop through the list of applications and install them using Chocolatey
+# Loop through the list of applications and install them using Winget
 foreach ($app in $applications) {
     Write-Host "Installing $app ..."
-    choco install $app -y
+    winget install $app
 }
 
 Write-Host "All applications installed successfully!"
