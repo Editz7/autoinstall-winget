d7471f719bc8c3a4b94cca3df84753267474dbd9
Editz7 <aeast7@protonmail.com>
2023-04-11 08:57:54 -0500

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -9,18 +9,8 @@ if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdenti
 try {
     winget --version | Out-Null
 } catch {
-    Write-Host "winget is not installed." -ForegroundColor Red
-    $installWinget = Read-Host "Would you like to install Windows Package Manager? (Y/N)"
-    if ($installWinget -eq "Y" -or $installWinget -eq "y") {
-        $wingetUrl = "https://github.com/microsoft/winget-cli/releases/download/v1.4.10173/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
-        Invoke-WebRequest -Uri $wingetUrl -OutFile "Microsoft.DesktopAppInstaller.msixbundle"
-        Add-AppPackage -Path "Microsoft.DesktopAppInstaller.msixbundle" | Out-Null
-        Remove-Item "Microsoft.DesktopAppInstaller.msixbundle"
-        Write-Host "winget has been installed." -ForegroundColor Green
-    } else {
-        Write-Host "Please install Windows Package Manager before running this script." -ForegroundColor Red
-        Exit
-    }
+    Write-Host "winget is not installed. Please install Windows Package Manager before running this script." -ForegroundColor Red
+    Exit
 }
 
 # List of preset applications to install
@@ -32,18 +22,11 @@ $appsToInstall = @(
     "Valve.Steam"
 )
 
-# Get the list of installed packages
-$installedPackages = winget list --id --name
-
-# Install each application from the list if not already installed
+# Install each application from the list
 foreach ($app in $appsToInstall) {
-    if ($installedPackages -match $app) {
-        Write-Host "$app is already installed." -ForegroundColor Green
-    } else {
-        Write-Host "Installing $app..." -ForegroundColor Yellow
-        winget install --id $app --accept-package-agreements --accept-source-agreements -e | Out-Null
-        Write-Host "$app has been installed." -ForegroundColor Green
-    }
+    Write-Host "Installing $app..." -ForegroundColor Yellow
+    winget install --id $app --accept-package-agreements --accept-source-agreements -e | Out-Null
+    Write-Host "$app has been installed." -ForegroundColor Green
 }
 
-Write-Host "All preset applications have been processed." -ForegroundColor Green
+Write-Host "All preset applications have been installed." -ForegroundColor Green
