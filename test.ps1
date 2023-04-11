4427dadf774ef84e2c5edee1b28e8f71bb1d42d6
Editz7 <aeast7@protonmail.com>
2023-04-11 08:44:43 -0500

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,27 +1,32 @@
-# Check if Winget is installed
-if (!(Get-Command winget -ErrorAction SilentlyContinue)) {
-    # Prompt the user to install Winget
-    $installWinget = Read-Host "Winget is not installed. Do you want to install it? (Y/N)"
-    if ($installWinget -eq "Y") {
-        # Download and install the Winget MSI from Microsoft's GitHub repository
-        $wingetMsiUrl = "https://github.com/microsoft/winget-cli/releases/latest/download/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
-        $wingetMsiPath = "$env:TEMP\winget.msixbundle"
-        Invoke-WebRequest $wingetMsiUrl -OutFile $wingetMsiPath
-        Add-AppPackage $wingetMsiPath -ErrorAction Stop
-        Write-Host "Winget installed successfully!"
-    } else {
-        Write-Host "Winget installation cancelled."
-        exit
-    }
+# Ensure the script runs with administrator privileges
+if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    $arguments = "& '" + $myinvocation.mycommand.definition + "'"
+    Start-Process powershell -Verb runAs -ArgumentList $arguments
+    Break
 }
 
-# Define a list of applications to install
-$applications = "7zip.7zip", "OBSProject.OBSStudio", "Discord.Discord", "Chromium.Chromium", "Telegram.TelegramDesktop"
-
-# Loop through the list of applications and install them using Winget
-foreach ($app in $applications) {
-    Write-Host "Installing $app ..."
-    winget install $app
+# Check if winget is installed
+try {
+    winget --version | Out-Null
+} catch {
+    Write-Host "winget is not installed. Please install Windows Package Manager before running this script." -ForegroundColor Red
+    Exit
 }
 
-Write-Host "All applications installed successfully!"
+# List of preset applications to install
+$appsToInstall = @(
+    "7zip.7zip",
+    "Discord.Discord",
+    "obsproject.obs-studio",
+    "Telegram.TelegramDesktop",
+    "Valve.Steam"
+)
+
+# Install each application from the list
+foreach ($app in $appsToInstall) {
+    Write-Host "Installing $app..." -ForegroundColor Yellow
+    winget install --id $app --accept-package-agreements --accept-source-agreements -e | Out-Null
+    Write-Host "$app has been installed." -ForegroundColor Green
+}
+
+Write-Host "All preset applications have been installed." -ForegroundColor Green
